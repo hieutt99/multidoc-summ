5a0d1a86bfd30c006776c78abdc516c47231237a
hieutt99 <hieutt99@gmail.com>
2022-05-27 22:32:05 +0700

    f

--- long_transformer/models/model_map.py
+++ long_transformer/models/model_map.py
@@ -1,9 +1,10 @@
 from .basic_models import *
+from .led_utils import *
 
 MODEL_MAP = {
     'basic_ext': BasicTransformerSentenceClassification, 
     'basic_abs': BasicTransformerSentenceGeneration,
 
-    
+    'led_ext' : LEDBasicSentenceClassificationModel,
 }
 

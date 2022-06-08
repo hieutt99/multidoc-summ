385c8e7ac273b023682cd93e465670d6bd2b3029
hieutt99 <hieutt99@gmail.com>
2022-06-09 00:10:10 +0700

    f

--- long_transformer/gdown_bert_data.sh
+++ long_transformer/gdown_bert_data.sh
@@ -1,3 +1,9 @@
-# https://drive.google.com/file/d/1-0ETt-hN4AXZnn-cIYxg6p80LkbqH4iu/view?usp=sharing
-gdown --id 1-0ETt-hN4AXZnn-cIYxg6p80LkbqH4iu
-unzip ./multi_news_bert_led_shards.zip
+# # https://drive.google.com/file/d/1-0ETt-hN4AXZnn-cIYxg6p80LkbqH4iu/view?usp=sharing
+# gdown --id 1-0ETt-hN4AXZnn-cIYxg6p80LkbqH4iu
+# unzip ./multi_news_bert_led_shards.zip
+
+
+# https://drive.google.com/file/d/1-A_6QnYDMUGG4nqmM6I8GrjALc1CHbBl/view?usp=sharing
+
+gdown --id 1-A_6QnYDMUGG4nqmM6I8GrjALc1CHbBl
+unzip ./abs_bert_data_shards.zip
--- long_transformer/models/basic_models.py
+++ long_transformer/models/basic_models.py
@@ -1,43 +1,62 @@
 from distutils.command.build import build
 import torch 
 import torch.nn as nn 
-from .modules.transformer_base import (BasicTransformerModel, 
+import copy
+from .modules.transformer_base import (TransformerDecoder, 
                                         BasicTransformerEncoder, 
                                         BasicTransformerEncoderBlock, 
                                         PositionwiseFeedForward,)
 from .bert import PositionalEncoding
 from .bert_utils import build_bert
 
+def get_generator(vocab_size, dec_hidden_size):
+    gen_func = nn.LogSoftmax(dim=-1)
+    generator = nn.Sequential(
+        nn.Linear(dec_hidden_size, vocab_size),
+        gen_func
+    )
+    return generator
+
 class BasicTransformerSentenceGeneration(nn.Module):
     def __init__(self, args, **kwargs):
         super(BasicTransformerSentenceGeneration, self).__init__()
         
-        self.bert = build_bert(args.bert_config) 
+        self.bert = build_bert(args.bert_model) 
         if args.freeze_bert:
             self.bert.eval()
         else: 
             self.bert.train()
+        self.bert.embeddings.register_buffer("position_ids", torch.arange(args.max_position_embeddings).expand((1, -1)))
+            
+        if(args.max_position_embeddings>258):
 
-        self.transformer_model = BasicTransformerModel(
-            d_model=args.d_model, 
-            num_heads=args.num_heads, 
-            num_encoder_blocks=args.num_encoder_blocks,
-            num_decoder_blocks=args.num_decoder_blocks, 
-            dim_ff=args.dim_ff,
-            dropout=args.dropout,
-            batch_first=args.batch_first, 
-            norm_first=args.norm_first, 
-            layer_norm_eps=args.layer_norm_eps
-        )
+            self.bert.embeddings.position_embeddings = nn.Embedding(args.max_position_embeddings, self.bert.config.hidden_size)
 
+        self.bert.embeddings.token_type_embeddings = nn.Embedding(2, self.bert.config.hidden_size)
 
+        self.pos_emb = PositionalEncoding(args.d_model, args.max_position_embeddings, args.dropout)
+        self.doc_type_embeddings = nn.Embedding(args.type_doc_size, args.d_model)
+
+        tgt_embeddings = nn.Embedding(args.vocab_size, args.d_model, padding_idx=0)
+        tgt_embeddings.weight = copy.deepcopy(self.bert.embeddings.word_embeddings.weight)
+
+        self.decoder = TransformerDecoder(
+            args.dec_layers,
+            args.dec_hidden_size, heads=args.dec_heads,
+            d_ff=args.dec_ff_size, dropout=args.dec_dropout, embeddings=tgt_embeddings)
+
+        self.generator = get_generator(args.vocab_size, args.d_model)
+        self.vocab_size = args.vocab_size
         self.args = args
 
-    def forward(self, src, tgt, segs, docs, mask_src, mask_tgt):
-        top_vec, _ = self.bert(src, attention_mask=mask_src, 
-                                token_type_ids=segs, doc_type_ids=docs)
+    def forward(self, src, tgt, segs, clss, mask_src, mask_tgt, mask_cls):
+        top_vec, _ = self.bert(input_ids=src,
+                            attention_mask=mask_src,
+                            token_type_ids=segs, return_dict=False)
         
-        return 
+        dec_state = self.decoder.init_decoder_state(src, top_vec)
+        decoder_outputs, state = self.decoder(tgt[:, :-1], top_vec, dec_state)
+        return decoder_outputs, None
 
 class BasicTransformerSentenceClassification(nn.Module):
     def __init__(self, args, **kwargs):

670a818aa439c132f06050883589c56ac4e2e8f4
hieutt99 <hieutt99@gmail.com>
2022-07-24 14:47:53 +0700

    f

--- long_transformer/old_configs/ultimate_led_ext/gdown_bert_data.sh
+++ long_transformer/old_configs/ultimate_led_ext/gdown_bert_data.sh
@@ -4,3 +4,8 @@
 # https://drive.google.com/file/d/1-81tUKva0b6qe4TZu-v_SgcHz5Brv_z9/view?usp=sharing
 gdown --id 1-81tUKva0b6qe4TZu-v_SgcHz5Brv_z9
 unzip ./multi_news_bert_led_ext_shards.zip
+
+# https://drive.google.com/file/d/1-4Ou95GY8jb3R12dmN320NMOWux0xviY/view?usp=sharing
+
+gdown --id 1-4Ou95GY8jb3R12dmN320NMOWux0xviY
+unzip ./multi_news_bert_led_ext_shards_5.zip ./multi_news_bert_led_ext_shards
